a0e0a380cf9750b3f627835f4d41e56ed8bde5b4
tweiland44 <tweiland44@f4e38ad5-1730-4f14-932f-3db32ab8bd7f>
2013-12-03 04:52:09 +0000

    Tim - Latest Updates R1.0

--- wsdm4/src/main/java/wsdm/util/Task.java
+++ wsdm4/src/main/java/wsdm/util/Task.java
@@ -1,14 +1,17 @@
 package wsdm.util;
 
 import java.util.*;
-
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import wsdm.domain.Sport;
 import wsdm.rest.SportRest;
 
 class Task extends TimerTask {
 	
 	private SportRest sportRest;
-	
+    private static final Logger LOGGER = 
+            Logger.getLogger(Task.class.getName());
+    
 	int size = 0;
 	int count = 0;
 	int exceptions = 0;
@@ -18,20 +21,19 @@ class Task extends TimerTask {
 	}
 
 	// run is a abstract method that defines task performed at scheduled time.
-	@SuppressWarnings("unchecked")
 	@Override
 	public void run() {
 
 
 		try {
-			Set<Sport> sports = (Set<Sport>) sportRest.listSports();
+			ArrayList<Sport> sports = (ArrayList<Sport>) sportRest.listSports();
 			size=sports.size();
 		} catch (Exception e) {
 			exceptions++;
 			e.printStackTrace();
 		}
 		count++;
-		// System.out.println("Task: "+count+" - tswacct.size= "+size);
+		LOGGER.log(Level.INFO, "wsdm Task: "+count+" - sports.size= "+size);
 	}
 	
 	public String getStatus(){
